0efa241cf875316001652e7e95d4a864714467f5
K0NSTANT1N3 <programming255.konst.end.01@gmail.com>
2025-06-13 12:34:43 +0400

    constants added

--- ikodave/src/main/java/com/example/registration/listener/ContextListener.java
+++ ikodave/src/main/java/com/example/registration/listener/ContextListener.java
@@ -7,6 +7,28 @@ import javax.servlet.ServletContextListener;
 public class ContextListener implements ServletContextListener {
     BasicDataSource dataSource;
 
+    @Override
+    public void contextInitialized(javax.servlet.ServletContextEvent sce) {
+        dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/registration");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+
+        // Store the DataSource in the servlet context for global access
+        sce.getServletContext().setAttribute("dataSource", dataSource);
+    }
+
+    @Override
+    public void contextDestroyed(javax.servlet.ServletContextEvent sce) {
+        try {
+            if (dataSource != null) {
+                dataSource.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error closing the database connection pool", e);
+        }
+    }
 
 
 }
---
+++ ikodave/src/main/java/com/example/registration/util/Constants.java
@@ -0,0 +1,11 @@
+package com.example.registration.util;
+
+public class Constants {
+    public static final String URL = "jdbc:mysql://localhost:3306/Ikodave";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DATABASE_USER = "mysqladmin";
+    public static final String DATABASE_PASSWORD = "mysqlroot";
+    public static final String USER_DAO_KEY = "userDao";
+
+
+}
