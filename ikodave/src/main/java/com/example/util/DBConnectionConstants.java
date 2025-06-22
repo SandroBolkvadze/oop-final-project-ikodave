1224502ce55dc8296e51ebc1140c9da792cd79cf
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2025-06-22 15:58:56 +0400

    minor fixes

--- ikodave/src/main/java/com/example/util/DBConnectionConstants.java
+++ ikodave/src/main/java/com/example/util/DBConnectionConstants.java
@@ -6,7 +6,4 @@ public class DBConnectionConstants {
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String DATABASE_USER = "root";
     public static final String DATABASE_PASSWORD = "root";
-    public static final String USER_DAO_KEY = "userDao";
-    public static final String PROBLEM_DAO_KEY = "ProblemDAO";
-    public static final String BASIC_DATASOURCE_KEY = "BasicDataSource";
 }
