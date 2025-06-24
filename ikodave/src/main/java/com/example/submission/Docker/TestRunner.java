0b85bb35906e2c9e0de0553b89e1bc677dab6d89
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2025-06-24 17:51:20 +0400

    added docker version of problem submitting

---
+++ ikodave/src/main/java/com/example/submission/Docker/TestRunner.java
@@ -0,0 +1,11 @@
+package com.example.submission.Docker;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestRunner {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ ikodave/src/main/java/com/example/submission/runners/DockerCodeRunner.java
@@ -0,0 +1,155 @@
+package com.example.submission.runners;
+
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class DockerCodeRunner {
+
+    private static final String userCode = """
+            import java.io.BufferedReader;
+            import java.io.IOException;
+            import java.io.InputStream;
+            import java.io.InputStreamReader;
+            import java.net.HttpURLConnection;
+            import java.net.URL;
+            import java.nio.file.Files;
+            import java.nio.file.Path;
+            import java.nio.file.Paths;
+            import java.util.List;
+            import java.util.concurrent.TimeUnit;
+        public class Solution {
+            public static int solve(List<Integer> nums) {
+                try {
+                    System.out.println("\\\\n[Test 3] Attempting to connect to http://example.com...");
+                    URL url = new URL("http://example.com");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.getResponseCode();
+                    System.out.println("==> FAILURE: Network connection succeeded.");
+                } catch (Exception e) {
+                    System.out.println("==> SUCCESS: Blocked from making a network connection. Error: " + e.getClass().getName() + ": " + e.getMessage());
+                }
+                return 0;
+            }
+        }
+        """;
+
+    private static final String testRunnerCode = """
+            import java.util.*;
+            public class TestRunner {
+                public static void main(String[] args) {
+                    List<Integer> input = Arrays.stream(args[0].split(","))                                               .map(Integer::parseInt)
+                                               .toList();
+                    System.out.println(Solution.solve(input));
+                }
+            }
+        """;
+
+
+    private static final Path WORKDIR = Paths.get(
+            System.getProperty("java.io.tmpdir"), "runner");
+
+    private final String IMAGE = "openjdk:21-slim";
+
+    public void runDockerProcess(String userCode, String testRunnerCode) throws IOException, InterruptedException {
+        Files.createDirectories(WORKDIR);
+        Files.writeString(WORKDIR.resolve("Solution.java"), userCode);
+        Files.writeString(WORKDIR.resolve("TestRunner.java"), testRunnerCode);
+
+        compileDocker();
+        runDocker("1,2,3", 2);
+        FileUtils.deleteDirectory(WORKDIR.toFile());
+    }
+
+    private void compileDocker() throws IOException, InterruptedException {
+        List<String> cmd = List.of(
+                "docker",
+                "run",
+                "--rm",
+                "-v", WORKDIR + ":/app",
+                "-w", "/app",
+                IMAGE,
+                "javac", "Solution.java", "TestRunner.java"
+        );
+
+        Process process = new ProcessBuilder(cmd)
+                .redirectErrorStream(true)
+                .start();
+
+        boolean finished = process.waitFor(10, TimeUnit.SECONDS);
+        int code = process.exitValue();
+
+        if (!finished) {
+            System.err.println("Compilation timed out");
+        } else if (code == 0) {
+            System.out.println("success compiling");
+        } else {
+            String out = new String(process.getInputStream().readAllBytes());
+            System.err.println("error compiling:\n" + out);
+        }
+    }
+
+    private void runDocker(String input, long timeoutMillis) throws IOException, InterruptedException {
+        List<String> cmd = List.of(
+                "docker",
+                "run",
+                "--rm",
+                "--read-only",
+                "--network=none",
+                "--memory=256m",
+                "--cpus=0.5",
+                "--cap-drop=ALL",
+                "--security-opt", "no-new-privileges",
+                "-v", WORKDIR + ":/app:ro",
+                "-w", "/app",
+                IMAGE,
+                "java", "-cp", ".", "TestRunner", input
+        );
+
+        Process process = new ProcessBuilder(cmd)
+                .redirectErrorStream(true)
+                .start();
+
+        boolean finished = process.waitFor(10, TimeUnit.SECONDS);
+        int code = process.exitValue();
+
+        if (!finished) {
+            System.err.println("Running timed out");
+        } else if (code == 0) {
+            System.out.println("success Running");
+        } else {
+            String out = new String(process.getInputStream().readAllBytes());
+            System.err.println("error running:\n" + out);
+        }
+
+        getRunResult(process.getInputStream());
+    }
+
+    private void getRunResult(InputStream inputStream) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder current = new StringBuilder();
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            current.append(line);
+        }
+        System.out.println(current);
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DockerCodeRunner dockerCodeRunner = new DockerCodeRunner();
+        dockerCodeRunner.runDockerProcess(userCode, testRunnerCode);
+
+    }
+
+}
