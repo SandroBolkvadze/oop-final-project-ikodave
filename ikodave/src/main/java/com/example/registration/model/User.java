f921a3c3eb76e8ff704dce4563d86c212df09328
K0NSTANT1N3 <kende23@freeuni.edu.ge>
2025-06-25 18:23:36 +0400

    fixes to servlets and dto converters, also updated user dto i suggest you guys checking what i have changet so that you wont mess up

--- ikodave/src/main/java/com/example/registration/model/User.java
+++ ikodave/src/main/java/com/example/registration/model/User.java
@@ -1,9 +1,13 @@
 package com.example.registration.model;
 
+import java.util.Date;
+
 public class User {
     int id;
     private String username;
     private String password;
+    private int rank_id;
+    private Date register_date;
 
     public User() {}
 
@@ -34,5 +38,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getRank_id() {
+        return rank_id;
+    }
+
+    public void setRank_id(int rank_id) {
+        this.rank_id = rank_id;
+    }
+
+    public Date getRegister_date() {
+        return register_date;
+    }
+
+    public void setRegister_date(Date register_date) {
+        this.register_date = register_date;
     }
 }
