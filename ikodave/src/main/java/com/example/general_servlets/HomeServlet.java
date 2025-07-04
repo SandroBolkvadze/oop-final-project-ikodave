2f97357eab2fc3742aa9adb0afab3f7d7f55a2f5
K0NSTANT1N3 <programming255.konst.end.01@gmail.com>
2025-07-04 14:04:34 +0400

    fixing html endpoint filter

---
+++ ikodave/src/main/java/com/example/general_servlets/HomeServlet.java
@@ -0,0 +1,16 @@
+package com.example.general_servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse respones) throws ServletException, IOException {
+        request.getRequestDispatcher("/index.html").forward(request, respones);
+    }
+}
