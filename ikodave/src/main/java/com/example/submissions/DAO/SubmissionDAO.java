be2b65121bd9462c538647f13c0969e54b855fd9
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2025-07-06 22:25:36 +0400

    quick add interface

--- ikodave/src/main/java/com/example/submissions/DAO/SubmissionDAO.java
+++ ikodave/src/main/java/com/example/submissions/DAO/SubmissionDAO.java
@@ -2,10 +2,14 @@ package com.example.submissions.DAO;
 
 import com.example.submissions.DTO.Submission;
 
+import java.util.List;
+
 public interface SubmissionDAO {
 
     int insertSubmission(Submission submission);
 
     void updateSubmission(Submission submission);
+
+    List<Submission> getSubmissionsBy(int userId, int problemId);
 
 }
