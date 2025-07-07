a8d17c51d19d59a353d5a085bee4457d27289913
poz3443 <nmetr23@freeuni.edu.ge>
2025-07-07 15:39:16 +0400

    minor addition

--- ikodave/src/main/java/com/example/util/DBConnectionConstants.java
+++ ikodave/src/main/java/com/example/util/DBConnectionConstants.java
@@ -5,6 +5,6 @@ public class DBConnectionConstants {
     public static final String URL = "jdbc:mysql://localhost:3306/Ikodave";
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String DATABASE_USER = "root";
-    public static final String DATABASE_PASSWORD = "root";
+    public static final String DATABASE_PASSWORD = "pozitron4";
 
 }
