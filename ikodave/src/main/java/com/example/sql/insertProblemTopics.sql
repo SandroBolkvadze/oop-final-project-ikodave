b9a59f8b55345310a360d074819d2d1fdae86b9d
K0NSTANT1N3 <programming255.konst.end.01@gmail.com>
2025-07-08 18:31:05 +0400

    few changes in front xml and databases

--- ikodave/src/main/java/com/example/sql/insertProblemTopics.sql
+++ ikodave/src/main/java/com/example/sql/insertProblemTopics.sql
@@ -1,5 +1,4 @@
-
-USE ikodave;
+USE Ikodave;
 INSERT INTO problem_topic (topic) VALUES ('Implementation');
 INSERT INTO problem_topic (topic) VALUES ('Array');
 INSERT INTO problem_topic (topic) VALUES ('Hash Table');
