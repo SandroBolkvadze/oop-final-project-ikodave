0775039d9455ddef6ae962f73df8ddaf5e256b0d
K0NSTANT1N3 <programming255.konst.end.01@gmail.com>
2025-07-09 13:52:35 +0400

    dao finished

--- ikodave/src/main/java/com/example/leaderboard/dao/SQLLeaderboardDAO.java
+++ ikodave/src/main/java/com/example/leaderboard/dao/SQLLeaderboardDAO.java
@@ -1,9 +1,18 @@
 package com.example.leaderboard.dao;
 
 import com.example.registration.model.User;
+import com.mysql.cj.exceptions.ClosedOnExpiredPasswordException;
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.leaderboard.utils.ToSQL.getUsersRanked;
+import static com.example.problems.utils.ToDTO.toUser;
 
 public class SQLLeaderboardDAO implements LeaderboardDAO {
     BasicDataSource dataSource;
@@ -13,8 +22,18 @@ public class SQLLeaderboardDAO implements LeaderboardDAO {
     }
 
     @Override
-    public List<User> getUsersByRank(){
-
-        return null;
+    public List<User> getUsersByRank() {
+        String sqlStatement = getUsersRanked();
+        try(Connection con = dataSource.getConnection()){
+            PreparedStatement preparedStatement = con.prepareStatement(sqlStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<User> users = new ArrayList<>();
+            while(resultSet.next()){
+                users.add(toUser(resultSet));
+            }
+            return users;
+        }catch (SQLException e){
+            throw new RuntimeException("Error querying users by rank", e);
+        }
     }
 }
