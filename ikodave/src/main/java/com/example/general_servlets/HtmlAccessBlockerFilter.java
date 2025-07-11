67bea218ab543921798f65096737b4a1604cbd75
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2025-07-11 23:19:52 +0400

    remove comments

--- ikodave/src/main/java/com/example/general_servlets/HtmlAccessBlockerFilter.java
+++ ikodave/src/main/java/com/example/general_servlets/HtmlAccessBlockerFilter.java
@@ -1,37 +1,37 @@
-//package com.example.general_servlets;
-//
-//import javax.servlet.*;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//import java.io.IOException;
-//
-//@WebFilter("*.html")
-//public class HtmlAccessBlockerFilter implements Filter {
-//
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//            throws IOException, ServletException {
-//
-//        HttpServletRequest req = (HttpServletRequest) request;
-//        HttpServletResponse res = (HttpServletResponse) response;
-//
-//        String uri = req.getRequestURI();
-//        if (req.getDispatcherType() == DispatcherType.REQUEST) {
-//            if (uri.contains("/shared_html/") || uri.endsWith("/index.html")) {
-//                chain.doFilter(request, response);
-//                return;
-//            }
-//            res.sendRedirect("/home");
-//            return;
-//        }
-//        chain.doFilter(request, response);
-//    }
-//
-//    @Override
-//    public void init(FilterConfig filterConfig) {}
-//
-//    @Override
-//    public void destroy() {}
-//}
+package com.example.general_servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("*.html")
+public class HtmlAccessBlockerFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String uri = req.getRequestURI();
+        if (req.getDispatcherType() == DispatcherType.REQUEST) {
+            if (uri.contains("/shared_html/") || uri.endsWith("/index.html")) {
+                chain.doFilter(request, response);
+                return;
+            }
+            res.sendRedirect("/home");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+}
